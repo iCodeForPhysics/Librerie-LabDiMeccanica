0558a3b39202cb400b2c60fa64b80489d3d6e7eb
Lorenzo <95830245+iCodeForPhysics@users.noreply.github.com>
2022-05-16 14:43:47 +0200

    Update my_lib_incertezze.py
    Pulite le docstring

--- lib_incertezze/my_lib_incertezze.py
+++ lib_incertezze/my_lib_incertezze.py
@@ -1,61 +1,62 @@
 
 
 
-########################################################################################################### 
-#  /$$    /$$                              /$$                                      /$$$$$$      /$$$$$$  #
-# | $$   | $$                             |__/                                     /$$$_  $$    /$$__  $$ #
-# | $$   | $$ /$$$$$$   /$$$$$$   /$$$$$$$ /$$  /$$$$$$  /$$$$$$$   /$$$$$$       | $$$$\ $$   |__/  \ $$ #
-# |  $$ / $$//$$__  $$ /$$__  $$ /$$_____/| $$ /$$__  $$| $$__  $$ /$$__  $$      | $$ $$ $$      /$$$$$/ #
-#  \  $$ $$/| $$$$$$$$| $$  \__/|  $$$$$$ | $$| $$  \ $$| $$  \ $$| $$$$$$$$      | $$\ $$$$     |___  $$ #
-#   \  $$$/ | $$_____/| $$       \____  $$| $$| $$  | $$| $$  | $$| $$_____/      | $$ \ $$$    /$$  \ $$ #
-#    \  $/  |  $$$$$$$| $$       /$$$$$$$/| $$|  $$$$$$/| $$  | $$|  $$$$$$$      |  $$$$$$//$$|  $$$$$$/ #
-#     \_/    \_______/|__/      |_______/ |__/ \______/ |__/  |__/ \_______/       \______/|__/ \______/  #
-########################################################################################################### 
+########################################################################################################
+#  /$$    /$$                              /$$                                      /$$$$$$  /$$   /$$ #
+# | $$   | $$                             |__/                                     /$$$_  $$| $$  | $$ #
+# | $$   | $$ /$$$$$$   /$$$$$$   /$$$$$$$ /$$  /$$$$$$  /$$$$$$$   /$$$$$$       | $$$$\ $$| $$  | $$ #
+# |  $$ / $$//$$__  $$ /$$__  $$ /$$_____/| $$ /$$__  $$| $$__  $$ /$$__  $$      | $$ $$ $$| $$$$$$$$ #
+#  \  $$ $$/| $$$$$$$$| $$  \__/|  $$$$$$ | $$| $$  \ $$| $$  \ $$| $$$$$$$$      | $$\ $$$$|_____  $$ #
+#   \  $$$/ | $$_____/| $$       \____  $$| $$| $$  | $$| $$  | $$| $$_____/      | $$ \ $$$      | $$ #
+#    \  $/  |  $$$$$$$| $$       /$$$$$$$/| $$|  $$$$$$/| $$  | $$|  $$$$$$$      |  $$$$$$//$$   | $$ #
+#     \_/    \_______/|__/      |_______/ |__/ \______/ |__/  |__/ \_______/       \______/|__/   |__/ #
+########################################################################################################
+
 
 
 
 from sympy import symbols, diff, sqrt
 
-def incRipetute(sd, n, ris, sb):
+def incRipetute(sd: float, n: int, ris: float, sb: float) -> float:
     """
     Permette di calcolare l'errore per misure ripetute
 
         Parametri: 
-            sd (float): Deviazione standard
-            n (int): Numero di misure
-            ris (float): Risoluzione dello strumento
-            sb (float): Incertezza di tipo B
+            sd: Deviazione standard
+            n: Numero di misure
+            ris: Risoluzione dello strumento
+            sb: Incertezza di tipo B
         Ritorna:
-            sigma (float): Incertezza per misure ripetute
+            sigma: Incertezza per misure ripetute
     """
     sigma=sqrt((sd/sqrt(n))**2 +(ris/sqrt(12))**2 + (sb)**2)
     return sigma
 
-def incSingole(sd, ris, sb):
+def incSingole(sd: float, ris: float, sb: float) -> float:
     """
     Permette di calcolare l'errore per misure singole
 
         Parametri: 
-            sd (float): Deviazione standard
-            ris (float): Risoluzione dello strumento
-            sb (float): Incertezza di tipo B
+            sd: Deviazione standard
+            ris: Risoluzione dello strumento
+            sb: Incertezza di tipo B
         Ritorna:
-            sigma (float): Incertezza per misure singole
+            sigma: Incertezza per misure singole
     """
     sigma=sqrt((sd)**2 + (ris/sqrt(12))**2 + (sb)**2)
     return sigma
 
-def incPropagate(f, x, y, z, xv=.0, yv=.0, zv=.0, s_x=.0, s_y=.0, s_z=.0): # Propagazione delle incertezze
+def incPropagate(f: symbols, x: symbols, y: symbols, z: symbols, xv: float=.0, yv: float=.0, zv: float=.0, s_x: float=.0, s_y: float=.0, s_z: float=.0) -> float:
     """
     Permette di calcolare la propagazione delle incertezze
 
         Parametri: 
             f (sympy equation): Formula su cui propagare le incertezze
-            x, y, z (sympy symbols): Incognite presenti nella formula
-            xv, yv, zv (float): Valori veri delle incognite
-            s_x, s_y, s_z (float): Errori delle incognite
+            x, y, z: Incognite presenti nella formula
+            xv, yv, zv: Valori veri delle incognite
+            s_x, s_y, s_z: Errori delle incognite
         Ritorna:
-            inc (float): Incertezza propagata
+            inc: Incertezza propagata
         Esempio:
             f = 3x+y
             incPropagate(f, x, y, z, 1.0, 2.0, 0, 0.5, 0.5, 0.0)) -> 1.5811
